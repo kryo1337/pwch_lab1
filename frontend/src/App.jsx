6892c954b85a754e3e0b5dd917d73df1f76af93f
Jakub Ostrowski <kryo1337@gmail.com>
2025-10-04 09:37:00 +0200

    typo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [operation, setOperation] = useState(null)
   const [history, setHistory] = useState([])
   const [waitingForSecondNumber, setWaitingForSecondNumber] = useState(false)
-  const API_URL = import.meta.env.VITE_API_URL || 'https://calculator-backend-pwch.azurewebsites.net';
+  const API_URL = import.meta.env.VITE_API_URL || 'lab1-backend.azurewebsites.net';
 
   useEffect(() => {
     fetchHistory()

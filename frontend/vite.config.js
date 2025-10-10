16ea01582492e9840d275109b9b9d4a581f4171e
Jakub Ostrowski <kryo1337@gmail.com>
2025-10-10 10:24:38 +0200

    lab2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        pure_funcs: ['console.log']
+      },
+      format: {
+        comments: false
+      }
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom']
+        }
+      }
+    },
+    sourcemap: false,
+    chunkSizeWarningLimit: 1000
+  }
 })
